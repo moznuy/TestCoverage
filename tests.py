11ecd0beaac2c361e978eaa5d46ebb2a9be03b4f
Serhii Charykov <serhii.charykov@gmail.com>
2019-05-30 11:12:09 +0300

    Fix coverage

--- tests.py
+++ tests.py
@@ -10,6 +10,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'whynot.settings')
 django.setup()
 
 from FyTest.models import TestMachine
+from FyTest import views
 
 
 class TestMethods(unittest.TestCase):
